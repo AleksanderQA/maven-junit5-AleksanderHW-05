43268cfb331769e5765c106ccf4a4a3eed37b47b
Aleksander <144763744+AleksanderQA@users.noreply.github.com>
2024-02-21 20:54:01 +0200

    Update Calculator.java

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,8 +1,7 @@
 package org.example;
 
 public class Calculator {
-    public static void main(String[] args) {
-    }
+
     public int sumOfTwoIntegers (int num1, int num2){
         return num1 + num2;
     }
